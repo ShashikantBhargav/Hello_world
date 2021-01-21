5ea2466fc981ff92bdb9e78d75cec79716183f74
ShashikantBhargav <75611743+ShashikantBhargav@users.noreply.github.com>
2021-01-21 19:29:46 +0530

    Add files via upload
    hello world using class

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,13 @@
-#a is a variale that contain value.
+"""#a is a variale that contain value.
 a = "Hello world"
 #print a
-print(a)
+print(a)"""
+
+
+# hello world using class
+class Myclass(object):#object is optional
+	def show(self):#self is a variable.
+		print("Hello world")
+#x is object
+x = Myclass()
+x.show()
